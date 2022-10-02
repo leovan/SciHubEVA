bd5d5b5a31f58b84ecdeff3cc9a31f54cc842c04
Leo <leo-van@hotmail.com>
2022-10-02 13:36:38 +0800

    Fix error fetching PDF URL when link is a relative path and error fetching PDF URL and PDF when get a non 200 response.

--- scihub_eva/api/scihub_api.py
+++ scihub_eva/api/scihub_api.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
 
-import re
-import tempfile
 import threading
 import time
 
 from enum import Enum, unique
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin
 from lxml import etree
-from pdfminer.pdfparser import PDFParser
-from pdfminer.pdfdocument import PDFDocument
 from tempfile import NamedTemporaryFile
 from pathlib import Path
 from PIL import Image, ImageOps
+from pathvalidate import sanitize_filename
 
 from PySide6.QtCore import QObject
 
@@ -62,57 +59,55 @@ class SciHubAPI(QObject, threading.Thread):
             self._captcha_answer = kwargs['captcha_answer']
 
         self._sess = get_session()
-        self._doi_pattern = re.compile(r'\b(10[.][0-9]{4,}(?:[.][0-9]+)*/(?:(?!["&\'])\S)+)\b')
-        self._illegal_filename_pattern = re.compile(r'[\/\\\:\*\?\"\<\>\|]')
 
-    def get_pdf_metadata(self, pdf):
-        temp_pdf_file = tempfile.TemporaryFile()
-        temp_pdf_file.write(pdf)
+    def fetch_pdf_url(self, query):
+        scihub_url = Preferences.get_or_default(NETWORK_SCIHUB_URL_KEY, NETWORK_SCIHUB_URL_DEFAULT)
+        self._logger.info(
+            self.tr('Using Sci-Hub URL: ') + '<a href="{scihub_url}">{scihub_url}</a>'.format(scihub_url=scihub_url))
 
-        metadata = {
-            'author': 'UNKNOWN_AUTHOR',
-            'title': 'UNKNOWN_TITLE',
-            'year': 'UNKNOWN_YEAR'
-        }
+        query_type = guess_query_type(query)
+        self._logger.info(self.tr('Query type: ') + query_type.upper())
 
-        pdf_parser = PDFParser(temp_pdf_file)
+        pdf_url = query
+        err = None
 
-        try:
-            pdf_doc = PDFDocument(pdf_parser)
-            pdf_metadata = pdf_doc.info[0]
+        if query_type != 'pdf':
+            try:
+                self._logger.info(self.tr('Fetching PDF URL ...'))
 
-            author = make_pdf_metadata_str(pdf_metadata.get('Author', ''))
-            if author and author != '':
-                metadata['author'] = author
+                pdf_url_response = self._sess.post(
+                    scihub_url, data={'request': query}, verify=False,
+                    timeout=Preferences.get_or_default(
+                        NETWORK_TIMEOUT_KEY, NETWORK_RETRY_TIMES_DEFAULT, type=int) / 1000.0)
 
-            title = make_pdf_metadata_str(pdf_metadata.get('Title', ''))
-            if title and title != '':
-                metadata['title'] = title
+                if pdf_url_response.status_code != 200:
+                    self._logger.error(self.tr('Error {}').format(pdf_url_response.status_code))
+                    self._logger.info(self.tr('You may need check it manually.'))
+                    err = SciHubAPIError.UNKNOWN
+                else:
+                    html = etree.HTML(pdf_url_response.content)
+                    article = html.xpath('//div[@id="article"]/embed[1]') or \
+                              html.xpath('//div[@id="article"]/iframe[1]') if html is not None else None
 
-            year = pdf_metadata_moddate_to_year(make_pdf_metadata_str(pdf_metadata.get('ModDate', '')))
-            if year and year != '':
-                metadata['year'] = year
-        except Exception as e:
-            pass
+                    if article and len(article) > 0:
+                        pdf_url = urlparse(article[0].attrib['src'], scheme='http').geturl()
+                        pdf_url = urljoin(pdf_url_response.url, pdf_url)
+                        pdf_url_html = '<a href="{pdf_url}">{pdf_url}</a>'.format(pdf_url=pdf_url)
 
-        temp_pdf_file.close()
+                        self._logger.info(self.tr('Got PDF URL: ') + pdf_url_html)
+                    else:
+                        err = SciHubAPIError.NO_VALID_PDF
 
-        return metadata
+                        self._logger.error(self.tr('Failed to get PDF URL!'))
+                        self._logger.info(self.tr('You may need check it manually.'))
+            except Exception as e:
+                err = SciHubAPIError.UNKNOWN
 
-    def guess_query_type(self, query):
-        if query.startswith('http') or query.startswith('https'):
-            if query.endswith('pdf'):
-                query_type = 'pdf'
-            else:
-                query_type = 'url'
-        elif query.isdigit():
-            query_type = 'pmid'
-        elif query.startswith('doi:') or self._doi_pattern.match(query):
-            query_type = 'doi'
-        else:
-            query_type = 'string'
+                self._logger.error(self.tr('Failed to get PDF URL!'))
+                self._logger.info(self.tr('You may need check it manually.'))
+                self._logger.error(str(e))
 
-        return query_type
+        return pdf_url, err
 
     def get_captcha_info(self, pdf_captcha_response):
         captcha_id, captcha_img_url = None, None
@@ -163,7 +158,11 @@ class SciHubAPI(QObject, threading.Thread):
             pdf_captcha_response.url, data={'answer': self._captcha_answer, 'id': captcha_id}, verify=False,
             timeout=Preferences.get_or_default(NETWORK_TIMEOUT_KEY, NETWORK_RETRY_TIMES_DEFAULT, type=int) / 1000.0)
 
-        if pdf_response.headers['Content-Type'] == 'application/pdf':
+        if pdf_response.status_code != 200:
+            self._logger.error(self.tr('Error {}').format(pdf_response.status_code))
+            self._logger.info(self.tr('You may need check it manually.'))
+            err = SciHubAPIError.UNKNOWN
+        elif pdf_response.headers['Content-Type'] == 'application/pdf':
             self._logger.info(self.tr('Angel [CAPTCHA] down!'))
             pdf = pdf_response.content
         else:
@@ -183,7 +182,11 @@ class SciHubAPI(QObject, threading.Thread):
                 timeout=Preferences.get_or_default(
                     NETWORK_TIMEOUT_KEY, NETWORK_RETRY_TIMES_DEFAULT, type=int) / 1000.0)
 
-            if pdf_response.headers['Content-Type'] == 'application/pdf':
+            if pdf_response.status_code != 200:
+                self._logger.error(self.tr('Error {}').format(pdf_response.status_code))
+                self._logger.info(self.tr('You may need check it manually.'))
+                err = SciHubAPIError.UNKNOWN
+            elif pdf_response.headers['Content-Type'] == 'application/pdf':
                 pdf = pdf_response.content
             elif pdf_response.headers['Content-Type'].startswith('text/html'):
                 self._logger.warn(self.tr('Angel [CAPTCHA] is coming!'))
@@ -191,55 +194,15 @@ class SciHubAPI(QObject, threading.Thread):
                 pdf = pdf_response
             else:
                 self._logger.error(self.tr('Unknown PDF Content-Type!'))
+                self._logger.info(self.tr('You may need check it manually.'))
         except Exception as e:
             err = SciHubAPIError.UNKNOWN
 
             self._logger.error(self.tr('Failed to get PDF!'))
+            self._logger.info(self.tr('You may need check it manually.'))
             self._logger.error(str(e))
 
         return pdf, err
-
-    def fetch_pdf_url(self, query):
-        scihub_url = Preferences.get_or_default(NETWORK_SCIHUB_URL_KEY, NETWORK_SCIHUB_URL_DEFAULT)
-        self._logger.info(
-            self.tr('Using Sci-Hub URL: ') + '<a href="{scihub_url}">{scihub_url}</a>'.format(scihub_url=scihub_url))
-
-        query_type = self.guess_query_type(query)
-        self._logger.info(self.tr('Query type: ') + query_type.upper())
-
-        pdf_url = query
-        err = None
-
-        if query_type != 'pdf':
-            try:
-                self._logger.info(self.tr('Fetching PDF URL ...'))
-
-                pdf_url_response = self._sess.post(
-                    scihub_url, data={'request': query}, verify=False,
-                    timeout=Preferences.get_or_default(
-                        NETWORK_TIMEOUT_KEY, NETWORK_RETRY_TIMES_DEFAULT, type=int) / 1000.0)
-
-                html = etree.HTML(pdf_url_response.content)
-                article = html.xpath('//div[@id="article"]/embed[1]') or \
-                          html.xpath('//div[@id="article"]/iframe[1]') if html is not None else None
-
-                if article and len(article) > 0:
-                    pdf_url = urlparse(article[0].attrib['src'], scheme='http').geturl()
-                    pdf_url_html = '<a href="{pdf_url}">{pdf_url}</a>'.format(pdf_url=pdf_url)
-
-                    self._logger.info(self.tr('Got PDF URL: ') + pdf_url_html)
-                else:
-                    err = SciHubAPIError.NO_VALID_PDF
-
-                    self._logger.error(self.tr('Failed to get PDF URL!'))
-                    self._logger.info(self.tr('You may need check it manually.'))
-            except Exception as e:
-                err = SciHubAPIError.UNKNOWN
-
-                self._logger.error(self.tr('Failed to get PDF URL!'))
-                self._logger.error(str(e))
-
-        return pdf_url, err
 
     def save_pdf(self, pdf, filename):
         pdf_name_formatter = Preferences.get_or_default(
@@ -249,8 +212,8 @@ class SciHubAPI(QObject, threading.Thread):
                 FILE_OVERWRITE_EXISTING_FILE_KEY, FILE_OVERWRITE_EXISTING_FILE_DEFAULT, type=bool):
             pdf_name_formatter += '_' + str(round(time.time() * 1000000))
 
-        pdf_metadata = self.get_pdf_metadata(pdf)
-        query_type = self.guess_query_type(self._query_input)
+        pdf_metadata = get_pdf_metadata(pdf)
+        query_type = guess_query_type(self._query_input)
 
         if query_type in ['doi', 'pmid']:
             pdf_metadata['id'] = self._query_input
@@ -266,7 +229,7 @@ class SciHubAPI(QObject, threading.Thread):
             self._logger.error(self.tr('Unsupported filename keywords: ') + pdf_name_formatter)
             return
 
-        pdf_name = self._illegal_filename_pattern.sub('-', pdf_name)
+        pdf_name = sanitize_filename(pdf_name, replacement_text='-')
         pdf_path = str(Path(Preferences.get(FILE_SAVE_TO_DIR_KEY)) / pdf_name)
 
         with open(pdf_path, 'wb') as fp:
--- scihub_eva/utils/api_utils.py
+++ scihub_eva/utils/api_utils.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 
 import re
+import tempfile
 
 from typing import List
+from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfdocument import PDFDocument
 from pdfminer.psparser import PSLiteral, PSKeyword
 
 
 RANGE_QUERY_PATTERN = re.compile(r'\{\d+\-\d+\}')
+DOI_PATTERN = re.compile(r'\b(10[.][0-9]{4,}(?:[.][0-9]+)*/(?:(?!["&\'])\S)+)\b')
 
 
 def is_range_query(query_input: str) -> bool:
@@ -30,6 +34,57 @@ def gen_range_query_list(query_input: str) -> List[str]:
         range_items = [str(i) for i in range(int(range_from), int(range_to) + 1)]
 
     return [query_input.replace(range_pattern, range_item) for range_item in range_items]
+
+
+def guess_query_type(query):
+    if query.startswith('http') or query.startswith('https'):
+        if query.endswith('pdf'):
+            query_type = 'pdf'
+        else:
+            query_type = 'url'
+    elif query.isdigit():
+        query_type = 'pmid'
+    elif query.startswith('doi:') or DOI_PATTERN.match(query):
+        query_type = 'doi'
+    else:
+        query_type = 'string'
+
+    return query_type
+
+
+def get_pdf_metadata(pdf) -> dict:
+    temp_pdf_file = tempfile.TemporaryFile()
+    temp_pdf_file.write(pdf)
+
+    metadata = {
+        'author': 'UNKNOWN_AUTHOR',
+        'title': 'UNKNOWN_TITLE',
+        'year': 'UNKNOWN_YEAR'
+    }
+
+    pdf_parser = PDFParser(temp_pdf_file)
+
+    try:
+        pdf_doc = PDFDocument(pdf_parser)
+        pdf_metadata = pdf_doc.info[0]
+
+        author = make_pdf_metadata_str(pdf_metadata.get('Author', ''))
+        if author and author != '':
+            metadata['author'] = author
+
+        title = make_pdf_metadata_str(pdf_metadata.get('Title', ''))
+        if title and title != '':
+            metadata['title'] = title
+
+        year = pdf_metadata_moddate_to_year(make_pdf_metadata_str(pdf_metadata.get('ModDate', '')))
+        if year and year != '':
+            metadata['year'] = year
+    except Exception as e:
+        pass
+
+    temp_pdf_file.close()
+
+    return metadata
 
 
 def make_pdf_metadata_str(value) -> str:
@@ -58,6 +113,8 @@ __all__ = [
     'RANGE_QUERY_PATTERN',
     'is_range_query',
     'gen_range_query_list',
+    'guess_query_type',
+    'get_pdf_metadata',
     'make_pdf_metadata_str',
     'pdf_metadata_moddate_to_year'
 ]
